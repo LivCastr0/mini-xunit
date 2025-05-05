9307e44a62973ab4fa6799ddbe92ec5a1b3cf174
Livia <livcras@hotmail.com>
2025-05-04 23:13:57 -0300

    4. Testando TestCase

--- test_framework.py
+++ test_framework.py
@@ -21,6 +21,7 @@ class TestCase:
     def tear_down(self):
         pass
 
+
 class TestResult:
 
     RUN_MSG = 'run'
@@ -43,36 +44,103 @@ class TestResult:
 
     def summary(self):
         return f'{self.run_count} {self.RUN_MSG}, ' \
-               f'{str(len(self.failures))} {self.FAILURE_MSG}, ' \
-               f'{str(len(self.errors))} {self.ERROR_MSG}'
-               
+               f'{len(self.failures)} {self.FAILURE_MSG}, ' \
+               f'{len(self.errors)} {self.ERROR_MSG}'
+
+class TestStub(TestCase):
+    def test_success(self):
+        assert True
+
+    def test_failure(self):
+        assert False
+
+    def test_error(self):
+        raise Exception
+
+class TestSpy(TestCase):
+    def __init__(self, name):
+        super().__init__(name)
+        self.was_run = False
+        self.was_set_up = False
+        self.was_tear_down = False
+        self.log = ""
+
+    def set_up(self):
+        self.was_set_up = True
+        self.log += "set_up "
+
+    def test_method(self):
+        self.was_run = True
+        self.log += "test_method "
+
+    def tear_down(self):
+        self.was_tear_down = True
+        self.log += "tear_down"
+
+class TestCaseTest(TestCase):
+    def set_up(self):
+        self.result = TestResult()
+
+    def test_result_success_run(self):
+        stub = TestStub('test_success')
+        stub.run(self.result)
+        assert self.result.summary() == '1 run, 0 failed, 0 error'
+
+    def test_result_failure_run(self):
+        stub = TestStub('test_failure')
+        stub.run(self.result)
+        assert self.result.summary() == '1 run, 1 failed, 0 error'
+
+    def test_result_error_run(self):
+        stub = TestStub('test_error')
+        stub.run(self.result)
+        assert self.result.summary() == '1 run, 0 failed, 1 error'
+
+    def test_result_multiple_run(self):
+        stub = TestStub('test_success')
+        stub.run(self.result)
+        stub = TestStub('test_failure')
+        stub.run(self.result)
+        stub = TestStub('test_error')
+        stub.run(self.result)
+        assert self.result.summary() == '3 run, 1 failed, 1 error'
+
+    def test_was_set_up(self):
+        spy = TestSpy('test_method')
+        spy.run(self.result)
+        assert spy.was_set_up
+
+    def test_was_run(self):
+        spy = TestSpy('test_method')
+        spy.run(self.result)
+        assert spy.was_run
+
+    def test_was_tear_down(self):
+        spy = TestSpy('test_method')
+        spy.run(self.result)
+        assert spy.was_tear_down
+
+    def test_template_method(self):
+        spy = TestSpy('test_method')
+        spy.run(self.result)
+        assert spy.log == "set_up test_method tear_down"
+
 if __name__ == "__main__":
-    class MyTest(TestCase):
-        def set_up(self):
-            print('set_up')
-
-        def tear_down(self):
-            print('tear_down')
-
-        def test_a(self):
-            print('test_a')
-
-        def test_b(self):
-            print('test_b')
-
-        def test_c(self):
-            print('test_c')
-
-    
     result = TestResult()
 
-    test = MyTest('test_a')
-    test.run(result)
+    tests = [
+        'test_result_success_run',
+        'test_result_failure_run',
+        'test_result_error_run',
+        'test_result_multiple_run',
+        'test_was_set_up',
+        'test_was_run',
+        'test_was_tear_down',
+        'test_template_method'
+    ]
 
-    test = MyTest('test_b')
-    test.run(result)
-
-    test = MyTest('test_c')
-    test.run(result)
+    for test_name in tests:
+        test = TestCaseTest(test_name)
+        test.run(result)
 
     print(result.summary())
